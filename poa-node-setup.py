9aed1fd612f25079a67be25f0495858a310a2fc9
Marat Pekker <mpekker@vmware.com>
2018-01-21 01:07:57 -0800

    moved menu functions to poaMenu module

--- poa-node-setup.py
+++ poa-node-setup.py
@@ -10,7 +10,7 @@ import os
 import sys
 import fileinput
 
-#import poa-devops
+#import poaMenu
 
 def getVarFromFile(filename):
 	import imp
@@ -22,82 +22,6 @@ def getVarFromFile(filename):
 def replace()
 	for i, line in enumerate(fileinput.input('all', inplace=1)):
 	sys.stdout.write(line.replace(old, new))
-
-def showMainMenu()
-	ans=True
-	while ans:
-		print ("""
-		1. Setup new node
-		2. Update node
-		3. Option 3
-		4.Exit/Quit
-		""")
-		ans=raw_input("What would you like to do? ") 
-		if ans=="1": 
-			print("\n OK, let's setup new node")
-			## show menu for node setup
-		elif ans=="2":
-			print("\n OK, let's update a node")
-			## show menu for node update
-		elif ans=="3":
-			print("\n Option 3")
-			## show menu for option 3
-		elif ans=="4":
-			print("\n Goodbye") 
-			## quit
-		elif ans !="":
-			print("\n Not a valid choice. Try again") 
-
-def showSetupNodeMenu
-	ans=True
-	while ans:
-		print ("""
-		1. 1
-		2. 2
-		3. 3
-		4.Exit/Quit
-		""")
-		ans=raw_input("...?") 
-		if ans=="1": 
-			print("\n 1")
-			## show menu for node setup
-		elif ans=="2":
-			print("\n 2")
-			## show menu for node update
-		elif ans=="3":
-			print("\n 3")
-			## show menu for option 3
-		elif ans=="4":
-			print("\n Goodbye")
-			## quit
-		elif ans !="":
-			print("\n Not a valid choice. Try again") 
-
-def showUpdateNodeMenu
-	ans=True
-	while ans:
-		print ("""
-		1. 1
-		2. 2
-		3. 3
-		4.Exit/Quit
-		""")
-		ans=raw_input("...?") 
-		if ans=="1": 
-			print("\n 1")
-			## show menu for node setup
-		elif ans=="2":
-			print("\n 2")
-			## show menu for node update
-		elif ans=="3":
-			print("\n 3")
-			## show menu for option 3
-		elif ans=="4":
-			print("\n Goodbye")
-			## quit
-		elif ans !="":
-			print("\n Not a valid choice. Try again") 
-
 			
 
 # Get all params from config.txt (later we will also add a menu)
---
+++ poaMenu.py
@@ -0,0 +1,74 @@
+def showMainMenu()
+	ans=True
+	while ans:
+		print ("""
+		1. Setup new node
+		2. Update node
+		3. Option 3
+		4.Exit/Quit
+		""")
+		ans=raw_input("What would you like to do? ") 
+		if ans=="1": 
+			print("\n OK, let's setup new node")
+			## show menu for node setup
+		elif ans=="2":
+			print("\n OK, let's update a node")
+			## show menu for node update
+		elif ans=="3":
+			print("\n Option 3")
+			## show menu for option 3
+		elif ans=="4":
+			print("\n Goodbye") 
+			## quit
+		elif ans !="":
+			print("\n Not a valid choice. Try again") 
+
+def showSetupNodeMenu
+	ans=True
+	while ans:
+		print ("""
+		1. 1
+		2. 2
+		3. 3
+		4.Exit/Quit
+		""")
+		ans=raw_input("...?") 
+		if ans=="1": 
+			print("\n 1")
+			## show menu for node setup
+		elif ans=="2":
+			print("\n 2")
+			## show menu for node update
+		elif ans=="3":
+			print("\n 3")
+			## show menu for option 3
+		elif ans=="4":
+			print("\n Goodbye")
+			## quit
+		elif ans !="":
+			print("\n Not a valid choice. Try again") 
+
+def showUpdateNodeMenu
+	ans=True
+	while ans:
+		print ("""
+		1. 1
+		2. 2
+		3. 3
+		4.Exit/Quit
+		""")
+		ans=raw_input("...?") 
+		if ans=="1": 
+			print("\n 1")
+			## show menu for node setup
+		elif ans=="2":
+			print("\n 2")
+			## show menu for node update
+		elif ans=="3":
+			print("\n 3")
+			## show menu for option 3
+		elif ans=="4":
+			print("\n Goodbye")
+			## quit
+		elif ans !="":
+			print("\n Not a valid choice. Try again") 
