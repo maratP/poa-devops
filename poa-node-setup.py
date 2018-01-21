480499761235fde088b77b836e2e138614e0d452
Marat Pekker <mpekker@vmware.com>
2018-01-21 00:30:01 -0800

    added main menu

--- poa-node-setup.py
+++ poa-node-setup.py
@@ -20,7 +20,33 @@ def getVarFromFile(filename):
 def replace()
 	for i, line in enumerate(fileinput.input('all', inplace=1)):
 	sys.stdout.write(line.replace(old, new))
+
+def showMenu()
+	ans=True
+	while ans:
+		print ("""
+		1. Setup new node
+		2. Update node
+		3. Option 3
+		4.Exit/Quit
+		""")
+		ans=raw_input("What would you like to do? ") 
+		if ans=="1": 
+			print("\n OK, let'setup new node")
+			## show menu for node setup
+		elif ans=="2":
+			print("\n OK, let's update a node")
+			## show menu for node update
+		elif ans=="3":
+			print("\n Option 3")
+			## shoe menu for option 3
+		elif ans=="4":
+			print("\n Goodbye") 
+			## quit
+		elif ans !="":
+			print("\n Not a valid choice. Try again") 
 	
+
 # Get all params from config.txt (later we will also add a menu)
 getVarFromFile('config.txt')
 
