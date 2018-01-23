b8eb8a9f096ed9c7fb4952970d5489a2daa0181f
Marat Pekker <mpekker@vmware.com>
2018-01-22 21:57:37 -0800

    Added logging for setup.log

--- poa-node-setup.py
+++ poa-node-setup.py
@@ -3,6 +3,13 @@
 import os
 import sys
 import fileinput
+import datetime
+import logging
+
+
+logFile = 'setup.log'
+logging.basicConfig( filename = logFile,filemode = 'w', level = logging.INFO,format = '%(asctime)s - %(levelname)s: %(message)s', datefmt = '%m/%d/%Y %I:%M:%S %p' )
+
 
 #import poaMenu
 
@@ -16,7 +23,6 @@ def getVarFromFile(fileName):
 def replace(oldStr,newStr,fileName):
 	for i, line in enumerate(fileinput.input(fileName, inplace=1)):
 		sys.stdout.write(line.replace(oldStr, newStr))
-			
 
 # Get all params from config.txt (later we will also add a menu)
 getVarFromFile('config.txt')
@@ -28,19 +34,24 @@ getVarFromFile('config.txt')
 
 ## Install ansible
 print ("Installing Ansible\n==========\n")
+logging.info('Installing Ansible')
 os.system ('sudo pip install ansible')
+
 
 ## Install packages
 print ("Installing boto and boto3\n==========\n")
+logging.info('Installing boto and boto3')
 os.system ('sudo pip install boto')
 os.system ('sudo pip install boto3')
 
 ## Make sure you have latest AWS CLI
 print ("Making sure you have latest AWS CLI\n==========\n")
+logging.info('Making sure you have latest AWS CLI')
 os.system ('pip install awscli --upgrade --user')
 
 ## Download and configure playbook
 print ("Downloading Ansible playbook\n==========\n")
+logging.info('Downloading Ansible playbook')
 os.chdir ('/home/ubuntu')
 os.system ('git clone https://github.com/poanetwork/deployment-playbooks.git')
 os.chdir ('deployment-playbooks')
@@ -49,8 +60,10 @@ os.chdir ('deployment-playbooks')
 # OR for sokol testnet
 #os.system ('git checkout sokol')
 
-print ("Selecting correct branch based on specified network type: [" + networkType + "]\n==========\n")
-cmd = "git checkout " + networkType
+print ("Selecting correct branch based on specified network type: [" + data.networkType + "]\n==========\n")
+text = "Selecting correct branch based on specified network type: [" + data.networkType + "]"
+logging.info(text)
+cmd = "git checkout " + data.networkType
 os.system (cmd)
 
 # check that you ended up on a correct branch (look where the `*` is)
@@ -58,14 +71,17 @@ os.system ('git branch')
 
 ## Prepare SSH keys (asummes you already have SSH keys for remote server)
 print ("Coping SSH keys\n==========\n")
+logging.info('Coping SSH keys')
 os.system ('cat ~/.ssh/id_rsa.pub > files/admins.pub')
 #os.system ('cp files/admins.pub files/ssh_validator.pub')
-cmd = "cp files/admins.pub files/ssh_" +nodeType+ ".pub"
+cmd = "cp files/admins.pub files/ssh_" +data.nodeType+ ".pub"
 #os.system ('cmd')
 
-print ("Configuring based on node type: [" +nodeType+ "]\n==========\n")
+print ("Configuring based on node type: [" +data.nodeType+ "]\n==========\n")
+text = 'Configuring based on node type: [' +data.nodeType+ ']'
+logging.info(text)
 #os.system ('cat group_vars/all.network group_vars/validator.example > group_vars/all')
-cmd = 'cat group_vars/all.network group_vars/'+nodeType+'.example > group_vars/all'
+cmd = 'cat group_vars/all.network group_vars/'+data.nodeType+'.example > group_vars/all'
 os.system (cmd)
 
 
@@ -78,7 +94,7 @@ os.system (cmd)
 os.chdir ('/home/ubuntu/deployment-playbooks/group_vars')
 
 print ("Updating files with your information...\n==========\n")
-
+logging.info("Updating files with your information...")
 #testFile=open(filename)
 fileName = "all"
 ##------------------------------------------------------------------
@@ -149,19 +165,22 @@ replace(oldStr,newStr,fileName)
 ## Create hosts file and add server IP
 ## We will need to add params here to specify correct node type
 print ("Creating HOSTS file\n==========\n")
+logging.info('Creating HOSTS file')
 os.chdir ('/home/ubuntu/deployment-playbooks')
 #cmd = "echo [validator] > hosts"
-cmd = "echo ["+nodeType+"] > hosts"
+cmd = "echo ["+data.nodeType+"] > hosts"
 os.system(cmd)
 cmd = "echo " + data.SERVER_IP + " >> hosts"
 os.system(cmd)
 
 # run this script to configure the instance (might want to use paramiko - ssh via python)
 print ("Running Ansible playbook and deploying\n==========\n")
+logging.info('Running Ansible playbook and deploying')
 os.system ('ansible-playbook -i hosts site.yml')
 
 
 print ("Done\n==========\n")
+logging.info('Done!')
 ## End of script
 
 #### Additional items for improvements:
