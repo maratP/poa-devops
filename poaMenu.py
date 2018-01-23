dc09974ca568135afae71e90f35fc042523a4461
Marat Pekker <mpekker@vmware.com>
2018-01-22 22:27:57 -0800

    Work in progress

--- poaMenu.py
+++ poaMenu.py
@@ -1,4 +1,6 @@
-def showMainMenu()
+#!/usr/bin/env python
+
+def showMainMenu():
 	ans=True
 	while ans:
 		print ("""
@@ -7,23 +9,74 @@ def showMainMenu()
 		3. Option 3
 		4.Exit/Quit
 		""")
-		ans=raw_input("What would you like to do? ") 
+		ans=raw_input("What would you like to do? \n") 
 		if ans=="1": 
 			print("\n OK, let's setup new node")
 			## show menu for node setup
+			showSetupNodeMenu()
 		elif ans=="2":
 			print("\n OK, let's update a node")
 			## show menu for node update
+			showUpdateNodeMenu()
 		elif ans=="3":
 			print("\n Option 3")
 			## show menu for option 3
 		elif ans=="4":
 			print("\n Goodbye") 
-			## quit
+			exit()
 		elif ans !="":
 			print("\n Not a valid choice. Try again") 
 
-def showSetupNodeMenu
+def showSetupNodeMenu():
+	ans=True
+	while ans:
+		print ("""
+		1. Core
+		2. Sokol
+		3.Exit/Quit
+		""")
+		ans=raw_input("Which network?") 
+		if ans=="1": 
+			print("\n OK, let's deploy new node on Core")
+			networkType = "core"
+			## show submenu
+			showSetupNodeMenu_1()
+		elif ans=="2":
+			print("\n OK, let's deploy new node on Sokol")
+			networkType = "sokol"
+			## show submenu
+			showSetupNodeMenu_1()
+		elif ans=="3":
+			print("\n Goodbye")
+			exit()
+		elif ans !="":
+			print("\n Not a valid choice. Try again") 
+			
+def showSetupNodeMenu_1():
+	ans=True
+	while ans:
+		print ("""
+		1. bootnode
+		2. validator
+		3. Master of Ceremony (MoC)
+		4. Exit/Quit
+		""")
+		ans=raw_input("...?") 
+		if ans=="1": 
+			print("\n OK, let's deploy bootnode")
+			## show menu for node setup
+		elif ans=="2":
+			print("\n OK, let's deploy validator node")
+			## show menu for node update
+		elif ans=="3":
+			print("\n OK, let's deploy Master of Ceremony node")
+		elif ans=="4":
+			print("\n Goodbye")
+			exit()
+		elif ans !="":
+			print("\n What type of node? \n") 
+
+def showUpdateNodeMenu():
 	ans=True
 	while ans:
 		print ("""
@@ -44,31 +97,11 @@ def showSetupNodeMenu
 			## show menu for option 3
 		elif ans=="4":
 			print("\n Goodbye")
-			## quit
+			exit()
 		elif ans !="":
-			print("\n Not a valid choice. Try again") 
-
-def showUpdateNodeMenu
-	ans=True
-	while ans:
-		print ("""
-		1. 1
-		2. 2
-		3. 3
-		4.Exit/Quit
-		""")
-		ans=raw_input("...?") 
-		if ans=="1": 
-			print("\n 1")
-			## show menu for node setup
-		elif ans=="2":
-			print("\n 2")
-			## show menu for node update
-		elif ans=="3":
-			print("\n 3")
-			## show menu for option 3
-		elif ans=="4":
-			print("\n Goodbye")
-			## quit
-		elif ans !="":
-			print("\n Not a valid choice. Try again") 
+			print("\n Not a valid choice. Try again \n")
+			
+			
+			
+			
+showMainMenu()
